fc8501691e94c2cd68143035283a73224b1f0e31
Liu Yue <hangxingliu@gmail.com>
2024-04-10 09:49:00 +0800

    Update wasm to fix the issue where it failed to ingest nested files/sheets from zip files

--- dist/types/src/bindings/runtime_browser_async_methods.d.ts
+++
@@ -1,8 +0,0 @@
-import { DuckDBModule } from './duckdb_module';
-import type { BROWSER_RUNTIME } from './runtime_browser';
-export declare function registerAsyncMethodsIntoBrowserRuntime(runtime: typeof BROWSER_RUNTIME): import("./runtime").DuckDBRuntime & {
-    _fileInfoCache: Map<number, import("./runtime").DuckDBFileInfo>;
-    _globalFileInfo: import("./runtime").DuckDBGlobalFileInfo | null;
-    getFileInfo(mod: DuckDBModule, fileId: number): import("./runtime").DuckDBFileInfo | null;
-    getGlobalFileInfo(mod: DuckDBModule): import("./runtime").DuckDBGlobalFileInfo | null;
-};
