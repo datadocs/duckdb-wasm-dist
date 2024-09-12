1aaa50f0e887e5592fb1bbdaa4e764f25c69b2a8
handstuyennn <tuyennn@ihands.com.vn>
2024-09-12 09:44:47 +0700

    Update wasm files for new version of duckdb

--- dist/types/src/bindings/config.d.ts
+++ dist/types/src/bindings/config.d.ts
@@ -67,4 +67,8 @@ export interface DuckDBConfig {
      * Whether to allow unsigned extensions
      */
     allowUnsignedExtensions?: boolean;
+    /**
+     * Custom user agent string
+     */
+    customUserAgent?: string;
 }
