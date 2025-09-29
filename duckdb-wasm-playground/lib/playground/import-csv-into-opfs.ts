d7f5395af5e3ff68bb2c781c8ed0dae7dd1a36a0
Liu Yue <hangxingliu@gmail.com>
2025-09-30 07:51:39 +0800

    Add one more small dataset for testing file ingest

--- duckdb-wasm-playground/lib/playground/import-csv-into-opfs.ts
+++ duckdb-wasm-playground/lib/playground/import-csv-into-opfs.ts
@@ -1,24 +1,45 @@
 import { LogsWriter } from "../utils/logs.js";
 
+// const DATASET = {
+//     DOWNLOAD_FROM:
+//         "https://www.kaggle.com/datasets/sonawanelalitsunil/crime-trends-2020present",
+//     LOCAL_URL: "/Crime_Data_from_2020_to_Present.csv",
+// } as const;
+
+const DATASET = {
+    DOWNLOAD_FROM:
+        "https://www.kaggle.com/datasets/saadaliyaseen/analyzing-student-academic-trends",
+    LOCAL_URL: "/student_exam_scores.csv",
+} as const;
+
 export async function onImportCSV(logs: LogsWriter) {
-    const url = "/Crime_Data_from_2020_to_Present.csv";
     const targetFile = "test.csv";
 
-    logs.addText(`import csv: ${url}`);
+    logs.addText(`import csv: ${DATASET.LOCAL_URL}`);
 
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`http error: ${response.status}`);
+    const resp = await fetch(DATASET.LOCAL_URL);
+    const contentType = resp.headers.get("content-type");
+    const contentLength = resp.headers.get("Content-Length");
 
-    const contentLength = response.headers.get("Content-Length");
+    if (!resp.ok || !contentType?.includes("/csv")) {
+        const errMsg = [
+            `failed to import csv file (status=${resp.status}, content-type=${contentType}`,
+            `please download this csv file from`,
+            `  ${DATASET.DOWNLOAD_FROM}`,
+            `to local file: \`duckdb-wasm-playground/public${DATASET.LOCAL_URL}\``,
+        ].join("\n");
+        throw new Error(errMsg);
+    }
+
     const totalSize = contentLength ? parseInt(contentLength, 10) : null;
     logs.addText(`total size: ${totalSize}`);
-    if (!response.body) throw new Error(`no response.body`);
+    if (!resp.body) throw new Error(`no response.body`);
 
     const root = await navigator.storage.getDirectory();
     const fileHandle = await root.getFileHandle(targetFile, { create: true });
     const writable = await fileHandle.createWritable();
     try {
-        const reader = response.body.getReader();
+        const reader = resp.body.getReader();
         let receivedSize = 0;
         let lastProgress = 0;
         while (true) {
@@ -29,7 +50,7 @@ export async function onImportCSV(logs: LogsWriter) {
             receivedSize += value.length;
             if (totalSize) {
                 const progress = Math.floor((receivedSize / totalSize) * 100);
-                if (progress >= (lastProgress + 10)) {
+                if (progress >= lastProgress + 10) {
                     logs.addText(`progress ${progress} %`);
                     lastProgress = progress;
                 }
--- duckdb-wasm-playground/public/README.md
+++ duckdb-wasm-playground/public/README.md
@@ -1,6 +1,10 @@
-# Dataset
+# Dataset Credits
 
 Crime_Data_from_2020_to_Present.csv
 
 <https://www.kaggle.com/datasets/sonawanelalitsunil/crime-trends-2020present>
 
+
+student_exam_scores.csv
+
+<https://www.kaggle.com/datasets/saadaliyaseen/analyzing-student-academic-trends>
